df3d0693b73433cebaef9a860efb8620da18e713
AhmetBuuyukaksoy <ahmetbuyukaksoy0@gramil.com>
2024-08-09 15:46:23 +0300

    Added data preprocessing

--- ingest.py
+++ ingest.py
@@ -1,6 +1,7 @@
 import logging
 from datasets import load_dataset
-import pandas as pd 
+import pandas as pd
+import string
 
 logging.basicConfig(level=logging.INFO)
 
@@ -13,4 +14,17 @@ print(dataset["train"][0]["text"])
 dataset["train"].to_parquet("data_pq")
 # Create dataframe to manipulate the data
 dataframe = pd.read_parquet("data_pq")
+
+
+# Function to preprocess text
+def preprocess_text(text):
+    text = text.lower()
+    # remove punctuation
+    text = text.translate(str.maketrans("", "", string.punctuation))
+    return text
+
+
+dataframe["text"] = dataframe["text"].apply(preprocess_text)
+#print(dataframe.head(10)["text"])
+
 
